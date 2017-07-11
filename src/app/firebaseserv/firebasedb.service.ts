129838247050f51f966c1e916741111764af2821
ec <varikvi@yahoo.com>
2017-07-11 22:44:18 +0200

    adding new service

--- src/app/firebaseserv/firebasedb.service.ts
+++ src/app/firebaseserv/firebasedb.service.ts
@@ -9,6 +9,7 @@ export class FirebasedbService {
   subjects: FirebaseListObservable<any[]>;
   platforms: FirebaseListObservable<any[]>;
   texts: FirebaseListObservable<any[]>;
+  userslist: FirebaseListObservable<any[]>;
 
   constructor(private db: AngularFireDatabase) {
     this.platforms = this.db.list('/platformstable');
@@ -16,14 +17,20 @@ export class FirebasedbService {
   
   
   getCategories() {
-     this.categories = this.db.list('/categories');
+     this.categories = this.db.list('/plt_categories');
      return this.categories;
  };
 
  getSubjects() {
-     this.subjects = this.db.list('/cv');
+     this.subjects = this.db.list('/fcc_subjects');
      return this.subjects;   
  }
+
+ getUserurls() {
+     this.userslist = this.db.list('/users');
+     return this.userslist;   
+ }
+
 
  //getPlatforms() {
  //    this.platforms = this.db.list('/platformstable');
