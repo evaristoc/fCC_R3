f277e19608899bbe520f2036f78da6d1ec4cc827
ec <varikvi@yahoo.com>
2017-07-12 13:22:40 +0200

    platforms listed in results.component

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input , OnInit} from '@angular/core';
 import { SearchFormComponent } from '../search-form/search-form.component';
-
+import { FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { FirebasedbService } from '../firebaseserv/firebasedb.service';
 
 @Component({
   selector: 'app-results',
@@ -9,16 +10,42 @@ import { SearchFormComponent } from '../search-form/search-form.component';
 })
 export class ResultsComponent implements OnInit {
   selectedSubjects = {};
+  selectedPlatforms = []
+  public platforms : FirebaseListObservable<any>;
   @Input('result') results: Array<any>;
   title= "You chose";
-  constructor() {
+  constructor(public db : FirebasedbService) {
+
+    this.platforms = this.db.platforms;
 
   }
 
   ngOnInit() {
     if (this.results){
-      console.log(this.results)
-    }  
+      //console.log(this.results);
+    }
+this.platforms.forEach((x) => {
+  x.forEach((a) => {
+    Object.keys(a).forEach((x) => {
+      var value = a[x]; 
+      if(value.category == this.results[1]){
+        console.log("PLATFORM : ", value.origurl,
+        "\n", 
+        "RELEVANCE (controlled) : ", typeof a.subjects != 'undefined' ? Object.keys(a.subjects[this.results[0][0]]).map((d) => {return a.subjects[this.results[0][0]][d]['count']})[0]:0,
+        "\n",
+        "POPULARITY (prevalence) : ", value.prevalence.reduce((a,b) => {return a+b}, 0)/11,
+        "\n\n"); 
+        this.selectedPlatforms.push([value.origurl, value.category])
+      }
+    })
+  })
+})
+
+    
+}
+
+showPlatforms(){
+  //console.log(this.platforms)
 }
 
 }
