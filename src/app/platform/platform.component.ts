c248549f6495096134e4ff5d9c2459b4bfe610b8
Sandra Dudley <sandra.dudley@ymail.com>
2017-07-15 17:53:40 +0000

    Read title and description of page

--- src/app/platform/platform.component.ts
+++ src/app/platform/platform.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { FirebasedbService } from '../firebaseserv/firebasedb.service';
 
 @Component({
   selector: 'app-platform',
@@ -9,12 +11,28 @@ import { ActivatedRoute } from '@angular/router';
 export class PlatformComponent implements OnInit, OnDestroy  {
   selection: string;
   private sub: any;
-  constructor(private route: ActivatedRoute) { }
+  public platforms : FirebaseListObservable<any>;
+  platform: object;
+  
+  constructor(private route: ActivatedRoute, public db : FirebasedbService) {
+    this.platforms = this.db.platforms;
+    
+  }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
-       this.selection = params['selection']; 
-
+      this.selection = params['selection']; 
+      this.platforms.forEach((x) => {
+          x.forEach((a) => {
+            Object.keys(a).forEach((x) => {
+              if (a.$key === this.selection && a[x].category) {
+                this.platform = a[x];
+                console.log(a[x]);
+              }
+            })
+          
+        })
+      })
     });
   }
   
