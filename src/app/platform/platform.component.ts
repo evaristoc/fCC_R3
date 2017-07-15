86da13c473964f219fdb978976815762d34e341a
Sandra Dudley <sandra.dudley@ymail.com>
2017-07-15 17:37:24 +0000

    Read params

--- src/app/platform/platform.component.ts
+++ src/app/platform/platform.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-platform',
   templateUrl: './platform.component.html',
   styleUrls: ['./platform.component.css']
 })
-export class PlatformComponent implements OnInit {
-
-  constructor() { }
+export class PlatformComponent implements OnInit, OnDestroy  {
+  selection: string;
+  private sub: any;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+       this.selection = params['selection']; 
+
+    });
+  }
+  
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
