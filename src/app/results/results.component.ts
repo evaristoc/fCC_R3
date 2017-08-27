a2c34e46c738e648cff3f50fc62ac085bffcceed
ec <varikvi@yahoo.com>
2017-08-27 23:06:59 +0200

    changes in a temp folder: working on issues #20, #22, #24, #26

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -11,65 +11,21 @@ import { FirebasedbService } from '../firebaseserv/firebasedb.service';
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent implements OnInit {
-  selectedSubjects = {};
-  platforms: any;
-  selectedPlatforms:Array<any>;
-  show:boolean = true;
-  public catfilters:Array<any>;
-  public filteredResults: Array<any> = [];
-  public start: number = 1;
   //public platforms : FirebaseListObservable<any>;
-  @Input('selectedsubj') selectedsubj: string;
+  //@Input('selectedsubj') selectedsubj: string;
   title= "You chose";
-  private categoryOptionslist: Array<any>;
-  public categoryOptions: any;
-  //public sevsubjects: Array<any>;
+
 
   constructor(public db : FirebasedbService) {
-    //this.platforms = this.db.platforms;
 
-     //const fireb_platforms$: FirebaseObjectObservable<any> = this.db.getPlatforms()
-     //fireb_platforms$.subscribe((plats)=>{return this.platforms = plats})
-
-
-    const fireb_categories$: FirebaseListObservable<any> = this.db.getCategories()
-    fireb_categories$.subscribe((cats)=>{return this.categoryOptionslist = cats[0]})
-    //flatted object
-    this.categoryOptions = this.categoryOptions.map((cat)=>{return {name: cat, isShown:true}},[])
-    console.log(this.categoryOptions)  
 }
   
   ngOnInit() {
-    
-    //console.log(this.platforms)
-    //this.catfilters = Object.keys(this.categoryOptions)
-    //this.setResults()
 
   }
 
  
-  setResults(){
-    //callback(filterset_result_dropdown);
-    //console.log("FILTERER in setResult", categories) //OUTPUT: FILTERER in setResult undefined and stop reading for SUBJECTS, but read for CATEGORIES
-      //this.selectedPlatforms = [];
-      let sortingplatforms:Array<any>;
-      for (let plat in this.platforms){
-        if(this.platforms[plat].subjects !== undefined){
-          let platdetails = this.selectedPlatforms[plat]
-            //console.log(Object.keys(platdetails))
-        Object.keys(platdetails).forEach((platdetailskey) => {
-          var platdetailsvalues = platdetails[platdetailskey]; 
-          if(platdetailsvalues.category){
-            var relevance = 1;
-            var prevalence = 1;
-            var ranking = 1;
-            if (relevance > 0) {
-              this.selectedPlatforms.push([platdetailsvalues.title, platdetailsvalues.category, relevance, prevalence, ranking, plat]);
-              console.log("TITLE", plat)
-            }
-          }
-        })
-      }
-    }
-  }
+ 
+
 }
+
--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -36,16 +36,16 @@ import { FirebasedbService } from '../firebaseserv/firebasedb.service';
 })
 export class SearchFormComponent implements OnInit {
   public selectedSubject: Array<any>;
-  private selPlatforms : FirebaseListObservable<any>;
-  public selplats : Array<any> = [];
   public showSubjectSection = false;
   public subjectOptions: Array<any> = [{name:"NOTHING"}];
   OPTIONTEST : any;
-  public catfilters:Array<any>;
-  public catOptions: Array<any> = [];
   public subj:string='';
   subjectModel: any;
 
+  private selPlatforms : FirebaseListObservable<any>;
+  public selplats : Array<any> = [];
+  public catfilters:Array<any>;
+  public catOptions: Array<any> = [];
 
   constructor(public db: FirebasedbService) {
 
@@ -57,19 +57,21 @@ export class SearchFormComponent implements OnInit {
     fireb_categories$.forEach((cats)=>{cats[0].forEach((c)=>{this.catOptions.push({category:c,isActive:true})})});
     
     this.selPlatforms = this.db.platforms;
+
   };
   
 
   ngOnInit() {
     console.log("subjectOptions in ngOnInit, searchform ", this.subjectOptions);
-    console.log("categoryOptionslist in ngOnInit, searchform ", this.catOptions);
-    console.log(this.selPlatforms)
     this.subjectModel = this.subjectOptions[0]
+      console.log("categoryOptionslist in ngOnInit, searchform ", this.catOptions);  
     this.selectedCat(this.catOptions);
-    this.setResults(this.catOptions,console.log)
+     this.setResults(this.catOptions,console.log)
+    console.log(this.selPlatforms)
+
   }
 
-  toggleCat( category ) {
+   toggleCat( category ) {
     let cat = this.catOptions.filter(
      (cats)=> {if (cats.category === category.category)
        {return cats}
@@ -122,22 +124,6 @@ setResults(filterset_result_dropdown, callback){
   callback(filterset_result_dropdown, this.selplats);
 }
 
-// outputResults(filterset_result_dropdown, selplats){
-//   //callback(filterset_result_dropdown);
-//   console.log("FILTERER in outputResult", filterset_result_dropdown) //OUTPUT: FILTERER in setResult undefined and stop reading for SUBJECTS, but read for CATEGORIES
-//   for(let sp = 0; sp < selplats.length; sp++){
-//     var selplat = selplats[sp]
-//     var fil = filterset_result_dropdown.filter((cat)=>{
-//         if(cat.category===selplat[2]){
-//           return cat.isActive
-//         }
-//       })[0];
-    
-//     selplats[sp] = selplat.slice(selplat.length-1)+[fil]  
-//   }
-// }
-
-
 outputR(selOp, sel){
   //callback(filterset_result_dropdown);
   //console.log("FILTERER in outputResult", filterset_result_dropdown) //OUTPUT: FILTERER in setResult undefined and stop reading for SUBJECTS, but read for CATEGORIES
@@ -147,7 +133,6 @@ outputR(selOp, sel){
       }
     })[0];
 }
-
 
   ngOnDestroy(){
     
